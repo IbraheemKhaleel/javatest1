4eda0999a6fdea2afda2ad0f61e0eeda8968676f
Ibraheem Khaleel <ikhaleelem@gmail.com>
2020-09-10 14:51:17 +0530

    added java subclass

---
+++ Exp2.java
@@ -0,0 +1,9 @@
+public class Exp2 extends Exp1
+{
+        public static void main(String args[])
+        {
+                Exp2 obj1 = new Exp2();
+                System.out.println("Length of a is " + obj1.a);
+        }
+}
+
